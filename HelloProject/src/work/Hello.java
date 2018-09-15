26b398933c05bbf88ac8845ef17169d87eb49b4d
kpjackson77 <kpjackson@cix.co.uk>
2018-09-15 15:30:01 +0100

    Use of Integer!

--- HelloProject/src/work/Hello.java
+++ HelloProject/src/work/Hello.java
@@ -2,6 +2,9 @@ package work;
 
 public class Hello{
 	public static void main(String[] args){
+		Integer a = 12, b = 76, c;
+		c = a + b;
 		System.out.println("Hello World!");
+		System.out.println("Answer: " + c);
 	}
 }
